661f80ef8aad13c3855e7c17da5f226e0932255e
Yuval Allweil <themarquisdesheric@gmail.com>
2022-11-06 21:29:11 -0800

    refactor to only use expressions

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const adder = ([first, ...rest]) => {
-  if (!first) return 0
-  
-  return first + adder(rest)
-}
+const adder = ([first, ...rest]) =>
+  !first
+    ? 0
+    : first + adder(rest)
 
 console.log(
   adder([1, 2, 3, 4, 5])
@@ -10,11 +9,10 @@ console.log(
 
 // 15
 
-const length = ([first, ...rest]) => {
-  if (!first) return 0
-
-  return 1 + length(rest)
-}
+const length = ([first, ...rest]) =>
+  !first
+    ? 0
+    : 1 + length(rest)
 
 console.log(
   length([1, 2, 3, 4, 5])
@@ -28,11 +26,10 @@ console.log(
 
 // 16
 
-const multiply = (n, m) => {
-  if (m === 0) return m
-
-  return n + multiply(n, m - 1)
-}
+const multiply = (n, m) =>
+  m === 0
+    ? m
+    : n + multiply(n, m - 1)
 
 console.log(
   multiply(2, 12)
@@ -40,24 +37,26 @@ console.log(
 
 // 24
 
-const greaterThan = (n, m) => {
-  if (n === 0) return false
-  if (m === 0) return true
-
-  return greaterThan(n - 1, m - 1)
-}
+const greaterThan = (n, m) =>
+  (n === 0)
+    ? false
+    : (m === 0)
+      ? true
+      : greaterThan(
+        n - 1,
+        m - 1
+      )
 
 console.log(
-  greaterThan(1, 5)
+  greaterThan(5, 5)
 )
 
 // false
 
-const iterate = ([first, ...rest]) => {
-  if (!first) return []
-
-  return [first].concat(iterate(rest))
-}
+const iterate = ([first, ...rest]) =>
+  !first
+    ? []
+    : [first].concat(iterate(rest))
 
 console.log(
   iterate(['this', 'is', 'a', 'sentence'])
@@ -65,20 +64,31 @@ console.log(
 
 // ['this', 'is', 'a', 'sentence']
 
-const removeItem = ([first, ...rest], searchTerm) => {
-  if (!first) return []
-
-  if (first === searchTerm) {
-    return removeItem(rest)
-  }
-
-  return [first].concat(
-    removeItem(rest, searchTerm)
-  )
-}
+const removeItem = ([first, ...rest], searchTerm) =>
+  !first
+    ? []
+    : (first === searchTerm)
+      ? removeItem(rest)
+      : [first].concat(removeItem(rest, searchTerm))
 
 console.log(
   removeItem(['this', 'is', 'another', 'bad', 'sentence'], 'bad')
 )
 
 // ['this', 'is', 'another', 'sentence']
+
+/* occur searches arrays (including nested ones) for an occurrence of `searchTerm` and returns true if found */
+const occur = ([first, ...rest], searchTerm) =>
+  !first
+    ? false
+    : (first === searchTerm)
+      ? true
+      : (Array.isArray(first))
+        ? occur(first, searchTerm) || occur(rest, searchTerm)
+        : occur(rest, searchTerm)
+
+console.log(
+  occur(['here', 'is', 'a', ['nested', ['list', ['containing', ['the', ['search_term']]]]], 'sentence'], 'search_term')
+)
+
+// true
